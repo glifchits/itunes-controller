ff8ffae3d240d992be6bf35df8b2362648f228e3
George Lifchits <george.lifchits@gmail.com>
2013-10-26 16:11:02 -0400

    set rating post request

--- site.py
+++ site.py
@@ -5,12 +5,13 @@ Created on Oct 25, 2013
 '''
 
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 import json
 from ituneslib import iTunes
 
 app = Flask(__name__)
 app.debug = True
+log = app.logger
 
 SUCC = 'success'
 
@@ -46,6 +47,16 @@ def get_song():
     itunes = iTunes()
     info = itunes.current_track()
     return json.dumps(info)
+
+
+@app.route('/set_rating', methods = ['POST'])
+def set_rating():
+    rating = request.values.get('rating')
+    log.info ('rating is %s' % rating)
+    itunes = iTunes()
+    itunes.set_rating(rating)
+    return str(request.values)
+
 
 
 if __name__ == "__main__":
