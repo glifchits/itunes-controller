f7ef79cc4d675c10be66e00bba8a4509dd90e49f
George Lifchits <george.lifchits@gmail.com>
2013-10-26 16:10:48 -0400

    set rating functionality

--- static/itunes.js
+++ static/itunes.js
@@ -1,41 +1,74 @@
+var drawRating = function() {
+}
 var songInfo = function() {
     $.getJSON('/get_song', function(data) {
+        console.debug(data)
         $('.nowplaying .artist').text(data.artist);
         $('.nowplaying .song').text(data.name);
         $('.nowplaying .album').text(data.album);
+
+        var i = 0;
+        $('.songstate .rating .star').each(function() {
+            if (i < data.rating / 20)
+                $(this).removeClass('no-star')
+            else
+                $(this).addClass('no-star')
+            i++;
+        });
+        drawRating();
     });
 }
-
-var rating = function() {
-    $('.rating .star').each(function() {
-        if ($(this).hasClass('no-star'))
-            icon = '<i class="fa fa-star-o"></i>';
-        else
-            icon = '<i class="fa fa-star"></i>';
-        $(this).html(icon);
-    })
+var setRating = function(obj) {
+    var ch = $(obj).children('.star');
+    $('.rating .star').hover(function() {
+        var idx = $(ch).index(this);
+        var i = 0;
+        $(ch).each(function() {
+            if (i <= idx)
+                $(this).removeClass('no-star')
+            else
+                $(this).addClass('no-star')
+            i++;
+        });
+    });
+    $('.rating .star').unbind('click').click(function() {
+        var idx = $(ch).index(this) + 1;
+        console.debug('star', idx, 'was clicked');
+        $.post('/set_rating', {
+            'rating' : idx * 20
+        });
+    });
+    drawRating();
 }
-
 $(function() {
     songInfo();
-    rating();
+    drawRating();
     $('.nowplaying').click(function() {
+        console.debug('refreshing song info');
         songInfo();
-        rating();
+        drawRating();
     })
     $('.toggle a').click(function() {
+        console.debug('play/pause song');
         $.post('/toggle');
         songInfo();
-        rating();
+        drawRating();
     })
     $('.prev a').click(function() {
+        console.debug('prev song');
         $.post('/prev');
         songInfo();
-        rating();
+        drawRating();
     })
     $('.next a').click(function() {
+        console.debug('next song');
         $.post('/next');
         songInfo();
-        rating();
+        drawRating();
+    })
+    $('.rating').hover(function() {
+        setRating(this);
+        songInfo();
+        drawRating();
     })
 });
